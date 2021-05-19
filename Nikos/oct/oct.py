cc9de3c5acde7fe1c128821254c6143da3833247
Nick-Trag <ntragkas@csd.auth.gr>
2021-05-19 03:25:04 +0300

    Figured out how to use a subset of the train set in oct.py. Will implement it tomorrow and finish

--- Nikos/oct/oct.py
+++ Nikos/oct/oct.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision import transforms
 
 from Nikos.oct.dataset import OCTDataset
@@ -31,7 +31,7 @@ def get_ranking(net, dataset, known_samples, device, batch_size=8):
     loader = DataLoader(dataset, batch_size=batch_size, num_workers=4)
     entropies = np.zeros(len(dataset) - known_samples)
     counter = known_samples
-    # TODO: Start from known_samples
+    # TODO: Start from known_samples. Use a sampler
     for i_batch, (images, labels) in enumerate(loader):
         images, labels = images.to(device), labels.to(device)
         outputs = net(images)
@@ -41,6 +41,8 @@ def get_ranking(net, dataset, known_samples, device, batch_size=8):
 
 def main():
     transform = transforms.Compose([
+        transforms.Resize((300, 300)),
+        # transforms.RandomCrop(250),
         transforms.ToTensor()
     ])
     train_set = OCTDataset(root_directory=os.path.join(data_root, 'OCT2017'), transform=transform, mode='train')
@@ -51,7 +53,14 @@ def main():
 
     known_samples = int(0.01 * train_set_size)
 
+    # Randomly choose the samples that we will consider labeled to start with
+    labeled_indices = np.random.choice(len(train_set), size=known_samples, replace=False)
+
+    train_set_l = Subset(train_set, labeled_indices)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    labeled_loader = DataLoader(train_set_l, batch_size=batch_size, shuffle=False)
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=False)
 
@@ -65,7 +74,7 @@ def main():
 
     # Train loop
     for epoch in range(epochs):
-        for i_batch, (images, labels) in enumerate(train_loader):
+        for i_batch, (images, labels) in enumerate(labeled_loader):
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = net(images)
@@ -73,9 +82,10 @@ def main():
             loss.backward()
             optimizer.step()
 
-            # TODO: Continue to next epoch when the known samples have been reached (note: i_batch counts batches, not individual images)
-
     torch.save(net.state_dict(), model_name)
+
+    # TODO: Now test it on the pool. Test it on the entire train set (no shuffle), but consider only the unlabeled ones. Then, add the indices to labeled_indices and recreate the subset and the loader
+    # (and save the accuracies)
 
 
 if __name__ == '__main__':
