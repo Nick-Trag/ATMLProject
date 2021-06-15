c08296c0655fcac3a4e3f62ba243c61b245e857c
chrigkou <34583795+chrigkou@users.noreply.github.com>
2021-06-15 20:13:16 +0300

    final updates and added readme
    added a readme and an additional classifier for evaluation

---
+++ Multilabel Learning/README.md.md
@@ -0,0 +1,8 @@
+# Multilabel Learning
+
+The dataset used is the [ Adverse Food Events](https://www.kaggle.com/fda/adverse-food-events) dataset found on Kaggle. It contains information on adverse event and product complaint reports submitted to FDA for foods, dietary supplements, and cosmetics. The goal is the prediction of the final outcome(s) after the use of the product.
+
+# Description
+
+In order to solve this problem two Problem  Transformation  Methods and one Algorithm  Adaptation  Method are used. The Problem  Transformation  Methods include Binary Relevance and Label Powerset, while the algorithms used with these include a Decision Tree, Gaussian Naive Bayes, K Nearest Neighbors Classifier and  SVM. The Problem Transformation Method used is Multi-label  KNN. 
+The predictions of the models are evaluated using label-based and example-based metrics.
--- Multilabel Learning/multilabel.py
+++ Multilabel Learning/multilabel.py
@@ -9,7 +9,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics import accuracy_score,precision_score,f1_score,recall_score
 from sklearn.metrics import hamming_loss, zero_one_loss, multilabel_confusion_matrix, classification_report
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from skmultilearn.adapt import MLkNN
@@ -29,21 +28,20 @@ def preprocess():
     print("Data length ", len(fda))
     print("Percentage of null values: ", (fda.isnull().sum()/fda.shape[0])*100)
     print("Shape of Training Dataset:", fda.shape)
-
     # number of appearances of each value
     gender = fda['CI_Gender'].value_counts()
     print(gender)
-    # drop not useful columns
+    # drop not useful columns or columns with a lot of missing values
     X = fda.drop(columns=['RA_Report #', 'RA_CAERS Created Date', 'AEC_Event Start Date', 'CI_Age at Adverse Event',
                           'CI_Age Unit', 'SYM_One Row Coded Symptoms'])
-    # drop rows with missing values on Coded Symptoms
+    # drop rows with missing values
     X = X.dropna()
-    # remove rows with not available values
+    # remove rows with not available values for gender
     X = X[(X.CI_Gender == "Female") | (X.CI_Gender == "Male")]
 
     # get the target column and convert to lower case
     labels = X['AEC_One Row Outcomes']
-    labels = labels.str.lower()
+    # labels = labels.str.lower()
 
     X = X.drop(columns=['AEC_One Row Outcomes'])
 
@@ -72,7 +70,7 @@ def preprocess():
                                     index=X.index))
     X = X.drop(columns=['PRI_Reported Brand/Product Name', 'product'])
 
-    print("Shape of new Training Dataset:", X.shape)
+    # print("Shape of new Training Dataset:", X.shape)
     return X, labels
 
 
@@ -81,12 +79,11 @@ def binary_relevance(X, labels):
     x_train, x_test, y_train, y_test = transform_and_split(X, labels)
 
     # transformation using binary relevance
-    classifier = BinaryRelevance(classifier=SVC(kernel='linear'), require_dense=[False, True])
+    classifier = BinaryRelevance(classifier=DecisionTreeClassifier(random_state=0), require_dense=[False, True])
     print("fitting...")
     classifier.fit(x_train, y_train)
     print("predicting...")
     y_pred = classifier.predict(x_test)
-    print(y_pred[0])
 
     return y_pred, y_test
 
@@ -96,12 +93,11 @@ def label_powerset(X, labels):
     x_train, x_test, y_train, y_test = transform_and_split(X, labels)
 
     # transformation using label powerset
-    classifier = LabelPowerset(classifier=DecisionTreeClassifier(random_state=0), require_dense=[False, True])
+    classifier = LabelPowerset(classifier=SVC(gamma='auto', random_state=0), require_dense=[False, True])
     print("fitting...")
     classifier.fit(x_train, y_train)
     print("predicting...")
     y_pred = classifier.predict(x_test)
-    print(y_pred[0])
 
     return y_pred, y_test
 
@@ -111,24 +107,21 @@ def knn(X, labels):
     x_train, x_test, y_train, y_test = transform_and_split(X, labels)
 
     # use multi label knn for the prediction
-    knn = MLkNN(k=10)
+    knn = MLkNN(k=3)
     print("fitting...")
     knn.fit(x_train, y_train)
     print("predicting...")
     y_pred = knn.predict(x_test)
-    print(y_pred[0])
 
     return y_pred, y_test
 
 
 def transform_and_split(X, labels):
-    #use multi label binarizer to transform the labels
+    # use multi label binarizer to transform the labels
     mlb = MultiLabelBinarizer()
-    print("Label example:")
-    print(labels[0])
     labels = mlb.fit_transform(labels.str.split(','))
-    print(labels[0])
-    print(list(mlb.classes_))
+    # print(labels[0])
+    # print(list(mlb.classes_))
     print('Number of unique labels: ', len(list(mlb.classes_)))
 
     x_train, x_test, y_train, y_test = train_test_split(X, labels, test_size=0.3, random_state=100)
@@ -140,9 +133,9 @@ def evaluation(y_test, y_pred):
     print("Hamming loss: ", hamming_loss(y_test, y_pred))
     print("Zero one loss: ", zero_one_loss(y_test,y_pred, normalize=True))
     print('Accuracy ', str(accuracy_score(y_test, y_pred)))
-    print('F1 score ', str(f1_score(y_test, y_pred, average='micro')))
-    print('Precision ', str(precision_score(y_test, y_pred, average='micro')))
-    print('Recall ', str(recall_score(y_test, y_pred, average='micro')))
+    print('F1 score ', str(f1_score(y_test, y_pred, average='samples')))
+    print('Precision ', str(precision_score(y_test, y_pred, average='samples')))
+    print('Recall ', str(recall_score(y_test, y_pred, average='samples')))
 
     #print('Confusion matrix: ', multilabel_confusion_matrix(y_test, y_pred))
     label_names = [' congenital anomaly', ' death', ' disability', ' hospitalization', ' life threatening', ' non-serious injuries/ illness',
@@ -154,10 +147,9 @@ def evaluation(y_test, y_pred):
 
 
 X, labels = preprocess()
-y_pred, y_test = binary_relevance(X, labels)
-#y_pred, y_test = label_powerset(X, labels)
+#y_pred, y_test = binary_relevance(X, labels)
+y_pred, y_test = label_powerset(X, labels)
 #y_pred, y_test = knn(X, labels)
 evaluation(y_test, y_pred)
-#label_based_macro_accuracy(y_test, y_pred)
-#label_based_micro_accuracy(y_test, y_pred)
-#label_based_macro_precision(y_test, y_pred)
+
+
